de6d05702e40b1c6de06a15de76aadf443be5609
egor <komarovegr@gmail.com>
2022-08-20 20:32:43 +0700

    Не хочет подключаться к БД.

--- DB/database.py
+++ DB/database.py
@@ -1,20 +1,15 @@
-import psycopg2
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
-connect_to_db_info = {
-    'login': 'postgres',
-    'password': 'postgres',
-    'database_name': 'vkinder'
-}
+from DB.models import create_tables
 
+DSN = sqlalchemy.engine.url.URL.create(drivername='postgresql+psycopg2', username='postgres', password='postgres',
+                                       host='localhost', port=5432, database='test')
 
-def create_dsn(login, password, database_name):
-    return 'postgresql://' + login + ':' + password + '@localhost:5432' + '/' + database_name
-
-
-DSN = create_dsn(**connect_to_db_info)
 engine = sqlalchemy.create_engine(DSN)
-Session = sessionmaker(bind=engine)
-session = Session()
-session.close()
+
+create_tables(engine)
+
+#Session = sessionmaker(bind=engine)
+#session = Session()
+#session.close()
--- DB/models.py
+++ DB/models.py
@@ -6,41 +6,57 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     surname = sq.Column(sq.Text, nullable=False)
     name = sq.Column(sq.Text, nullable=False)
     patronymic = sq.Column(sq.Text, nullable=True)
     age = sq.Column(sq.Integer, nullable=False)
     id_gender = sq.Column(sq.Integer, sq.ForeignKey('gender.id'), nullable=False)
     id_city = sq.Column(sq.Integer, sq.ForeignKey('city.id'), nullable=False)
+    city = relationship("City", backref='user')
+    gender = relationship('Gender', backref='user')
+    hobby = relationship('Hobby', backref='user')
+    black_list = relationship('Black_list', backref='user')
+    favorites = relationship('Favorites', backref='user')
 
 
 class FoundUser(Base):
     __tablename__ = 'found_user'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     surname = sq.Column(sq.Text, nullable=False)
     name = sq.Column(sq.Text, nullable=False)
     patronymic = sq.Column(sq.Text, nullable=True)
     age = sq.Column(sq.Integer, nullable=False)
     id_gender = sq.Column(sq.Integer, sq.ForeignKey('gender.id'), nullable=False)
     id_city = sq.Column(sq.Integer, sq.ForeignKey('city.id'), nullable=False)
+    city = relationship('City', backref='found_user')
+    gender = relationship('Gender', backref='found_user')
+    hobby = relationship('Hobby', backref='found_user')
+    black_list = relationship('Black_list', backref='found_user')
+    favorites = relationship('Favorites', backref='found_user')
+    photo = relationship('Photo', backref='found_user')
 
 
 class City(Base):
     __tablename__ = 'city'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     city_name = sq.Column(sq.Text, nullable=False)
 
 
 class Gender(Base):
     __tablename__ = 'gender'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     gender_name = sq.Column(sq.Text, nullable=False)
 
 
 class Hobby(Base):
     __tablename__ = 'hobby'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     hobby_name = sq.Column(sq.Text, nullable=False)
     id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id'), nullable=True)
     id_found_user = sq.Column(sq.Integer, sq.ForeignKey('found_user.id'), nullable=True)
@@ -48,21 +64,29 @@ class Hobby(Base):
 
 class Black_list(Base):
     __tablename__ = 'black_list'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id'), nullable=True)
     id_found_user = sq.Column(sq.Integer, sq.ForeignKey('found_user.id'), nullable=True)
 
 
 class Favorites(Base):
     __tablename__ = 'favorites'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     id_user = sq.Column(sq.Integer, sq.ForeignKey('user.id'), nullable=True)
     id_found_user = sq.Column(sq.Integer, sq.ForeignKey('found_user.id'), nullable=True)
 
 
 class Photo(Base):
     __tablename__ = 'photo'
-    id = sq.Column(sq.Integer, primary_key=True, unique=True)
+
+    id = sq.Column(sq.Integer, primary_key=True, unique=True, autoincrement=True)
     link = sq.Column(sq.Text, nullable=False)
     amount = sq.Column(sq.Integer, nullable=True)
     id_found_user = sq.Column(sq.Integer, sq.ForeignKey('found_user.id'), nullable=True)
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
