101f85ae65e5acc6ba542507ebef4d618062b95a
Steshin <warheim24@gmail.com>
2022-09-01 17:00:13 +0300

    lots of info

--- VK/vkontakte.py
+++ VK/vkontakte.py
@@ -3,6 +3,7 @@ import vk_api
 import datetime
 from Bot.vk_bot import get_id
 from pprint import pprint
+from string import punctuation as symbols
 
 
 class Vkontakte:
@@ -12,27 +13,91 @@ class Vkontakte:
         self.id = get_id(token)
         self.session = vk_api.VkApi(token=token)
         self.session_api = self.session.get_api()
+        self.customer_portrait = None
 
     def get_customer_info(self):
         user_id = self.id
-        response = self.session_api.users.get(user_ids=user_id, fields='city, sex, bdate, interests, personal')
-        customer_city = response[0]['city']['id']
-        customer_sex = response[0]['sex']
-        customer_birth_day = datetime.datetime.strptime(response[0]['bdate'], '%d.%m.%Y').date()
-        customer_age = int((datetime.date.today() - customer_birth_day).days // 365.25)
-        customer_interests = response[0]['interests']
-        customer_alcohol = response[0]['personal']['alcohol']
-        customer_inspiration = response[0]['personal']['inspired_by']
-        customer_people_main = response[0]['personal']['people_main']
-        customer_smoking = response[0]['personal']['smoking']
-        cutomer_portrait = {
-            'customer_city': customer_city,
-            'customer_sex': customer_sex,
-            'customer_age': customer_age,
-            'customer_interests': customer_interests,
-            'customer_alcohol': customer_alcohol,
-            'customer_inspiration': customer_inspiration,
-            'customer_people_main': customer_people_main,
-            'customer_smoking': customer_smoking
-        }
-        return pprint(cutomer_portrait)
+        response = self.session_api.users.get(user_ids=13977076, fields='city, sex, bdate, interests, personal, music,'
+                                                                       'movies, books')
+        customer_portrait = {}
+
+        if 'city' in response[0]:
+            customer_portrait['city'] = response[0]['city']['id']
+        else:
+            customer_portrait['city'] = None
+
+        if 'sex' in response[0]:
+            if response[0]['sex'] in [1, 2]:
+                customer_portrait['sex'] = response[0]['sex']
+            else:
+                customer_portrait['sex'] = None
+        if 'bdate' in response[0]:
+            if len(response[0]['bdate'].split('.')) == 3:
+                customer_birth_day = datetime.datetime.strptime(response[0]['bdate'], '%d.%m.%Y').date()
+                customer_portrait['age'] = int((datetime.date.today() - customer_birth_day).days // 365.25)
+            else:
+                customer_portrait['age'] = None
+
+        if 'interests' in response[0]:
+            customer_interests = response[0]['interests'].lower()
+            for item in customer_interests:
+                if item in symbols or item == ' ' and item != ',':
+                    customer_interests = customer_interests.replace(item, ',')
+            customer_interests = customer_interests.split(',')
+            temp_new_interests = []
+            for item in customer_interests:
+                if len(item) > 1:
+                    temp_new_interests.append(item)
+            customer_portrait['interests'] = temp_new_interests
+        else:
+            customer_portrait['interests'] = None
+
+        if 'books' in response[0]:
+            customer_books = response[0]['books'].lower()
+            for item in customer_books:
+                if item in symbols and item != ',':
+                    customer_books = customer_books.replace(item, ',')
+            customer_books = customer_books.split(',')
+            temp_new_books = []
+            for num in range(len(customer_books)):
+                customer_books[num] = customer_books[num].strip()
+                if len(customer_books[num]) > 1:
+                    temp_new_books.append(customer_books[num])
+            customer_portrait['books'] = temp_new_books
+        else:
+            customer_portrait['books'] = None
+
+        if 'personal' in response[0]:
+            if 'alcohol' in response[0]['personal']:
+                customer_portrait['alcohol'] = response[0]['personal']['alcohol']
+            else:
+                customer_portrait['alcohol'] = None
+        else:
+            customer_portrait['alcohol'] = None
+
+        if 'personal' in response[0]:
+            if 'inspired_by' in response[0]['personal']:
+                customer_portrait['inspiration'] = response[0]['personal']['inspired_by'].lower().split(', ')
+            else:
+                customer_portrait['inspiration'] = None
+        else:
+            customer_portrait['inspiration'] = None
+
+        if 'personal' in response[0]:
+            if 'people_main' in response[0]['personal']:
+                customer_portrait['people_main'] = response[0]['personal']['people_main']
+            else:
+                customer_portrait['people_main'] = None
+        else:
+            customer_portrait['people_main'] = None
+
+        if 'personal' in response[0]:
+            if 'smoking' in response[0]['personal']:
+                customer_portrait['smoking'] = response[0]['personal']['smoking']
+            else:
+                customer_portrait['smoking'] = None
+        else:
+            customer_portrait['smoking'] = None
+
+        self.customer_portrait = customer_portrait
+        return pprint(self.customer_portrait)
